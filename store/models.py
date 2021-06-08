c5c50829e10956459ed6cff2d34f216a6d3e6712
Sirojiddin-Kh <sirojiddinhamidov2001@gmial.com>
2021-06-08 23:39:55 +0500

    product detail page and getting the books according to the category

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -8,6 +9,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "categories"
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -30,6 +34,9 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = "Products"
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 from . models import Category, Product
 
 
@@ -16,4 +16,19 @@ def all_products(request):
     }
     return render(request, 'store/home.html', context)
 
+
 def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    context = {
+        'product': product
+    }
+    return render(request, 'store/products/detail.html', context)
+
+
+def category_list(request, category_list):
+    category = get_object_or_404(Category, slug=category_list)
+    products = Product.objects.filter(category=category)
+    context = {
+        'products': products
+    }
+    return render(request, 'store/products/category.html', context)
