2a712fd3f3320ffcd2cbf1b90cd328bfcf1146ea
Sirojiddin-Kh <sirojiddinhamidov2001@gmial.com>
2021-06-08 22:58:04 +0500

    worked with home tempalte

--- store/views.py
+++ store/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from . models import Category, Product
 
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
 def all_products(request):
     products = Product.objects.all()
     context = {
         'products': products
     }
     return render(request, 'store/home.html', context)
+
+def product_detail(request, slug):
