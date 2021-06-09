4969cc381ffd9081c254ea73a99fb5c4fd411555
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-06-09 18:16:34 +0500

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -93,5 +93,10 @@ STATIC_URL = '/static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+print("hello")
