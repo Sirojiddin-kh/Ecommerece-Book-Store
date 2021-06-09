8f4461157dfdc12514c6162ef2636dc60e7d9b69
Sirojiddin-Kh <sirojiddinhamidov2001@gmial.com>
2021-06-09 17:35:12 +0500

    Refactoring files and some function, url names, worked with Managers in models

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.all_products, name='all_products'),
-    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('search/<slug:category_slug>/', views.category_list, name='category_list')
+    path('', views.product_all, name='product_all'),
+    path('book/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('shop/<slug:category_list>/', views.category_list, name='category_list')
 ]
--- store/views.py
+++ store/views.py
@@ -3,14 +3,8 @@ from django.shortcuts import get_object_or_404
 from . models import Category, Product
 
 
-def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
-
-
-def all_products(request):
-    products = Product.objects.all()
+def product_all(request):
+    products = Product.products.all()
     context = {
         'products': products
     }
