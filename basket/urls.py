9cf1f51ce9ffb0a7b4976caa2b0336df86383028
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-06-10 15:51:02 +0500

    Quantity of items

--- basket/urls.py
+++ basket/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
-    path('', views.basket_add, name='basket_add')
+    path('', views.basket_add, name='basket_add'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from . basket import Basket
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
 from store.models import Product
+
+from .basket import Basket
 
 
 def basket_summary(request):
-    return render(request, 'basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'basket/summary.html', {'basket': basket})
 
 
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
-        product = get_object_or_404(Product, id = product_id)
-        basket.add(product=product)
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response
